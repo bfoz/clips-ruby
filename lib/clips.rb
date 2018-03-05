81fcb592335d0cde2c76e0022b2eefbcc69e9783
Brandon Fosdick <bfoz@bfoz.net>
2018-03-05 10:08:41 -0800

    bundle gem clips

---
+++ lib/clips.rb
@@ -0,0 +1,5 @@
+require "clips/version"
+
+module Clips
+  # Your code goes here...
+end
---
+++ spec/clips_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Clips do
+  it "has a version number" do
+    expect(Clips::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
